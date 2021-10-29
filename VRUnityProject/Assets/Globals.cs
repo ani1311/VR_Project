4728a0b6e97322282958b5aca5ea71c528cc6768
Bharath Tej Chinimilli <bharathtej@Bharaths-MBP.lan>
2021-10-28 22:38:54 -0500

    added longitude and longitude

--- VRUnityProject/Assets/Globals.cs
+++ VRUnityProject/Assets/Globals.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Globals : MonoBehaviour
 {
     public static List<Friend> friends;
+    public float myLatitude;
+    public float myLongitude;
     
     // Implies 1 Unit = 1 m
     public const double Scale = 10;
